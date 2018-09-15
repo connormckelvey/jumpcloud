765184fba5ffe4eea798f8bead1e3fb095d6f51c
Connor McKelvey <connormckelvey@gmail.com>
2018-09-15 00:06:10 -0600

    Remove unneeded wrapper methods by embedding hash.Hash

--- pkg/stringhasher/sha512/sha512.go
+++ pkg/stringhasher/sha512/sha512.go
@@ -10,40 +10,20 @@ import (
 )
 
 type sha512StringHasher struct {
-	hash hash.Hash
+	hash.Hash
 }
 
 // New returns a new stringhasher.StringHasher computing the SHA-512 checksum.
 func New() sh.StringHasher {
 	return &sha512StringHasher{
-		hash: sha512.New(),
+		sha512.New(),
 	}
 }
 
-func (h *sha512StringHasher) Write(b []byte) (n int, err error) {
-	return h.hash.Write(b)
-}
-
 func (h *sha512StringHasher) WriteString(s string) (n int, err error) {
-	return io.WriteString(h.hash, s)
-}
-
-func (h *sha512StringHasher) Sum(b []byte) []byte {
-	return h.hash.Sum(b)
-}
-
-func (h *sha512StringHasher) Reset() {
-	h.hash.Reset()
-}
-
-func (h *sha512StringHasher) Size() int {
-	return h.hash.Size()
-}
-
-func (h *sha512StringHasher) BlockSize() int {
-	return h.hash.BlockSize()
+	return io.WriteString(h.Hash, s)
 }
 
 func (h *sha512StringHasher) String() string {
-	return base64.StdEncoding.EncodeToString(h.hash.Sum(nil))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
